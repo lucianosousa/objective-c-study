4f5058fe7b06cde61ef3e097df619bd406da3611
Luciano Sousa <luciano@bonera.com.br>
2011-12-25 20:48:23 -0200

    added string example

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -9,7 +9,11 @@ int main (int argc, const char * argv[])
         float value1 = 50.5f;
         int value2 = 20;
         sum = value1 + value2;
+        NSString *firstString = @"'My first String'";
+        
         NSLog(@"The sum of the %f and %i is: %f", value1, value2, sum);
+        
+        NSLog(@"Created a string: %@", firstString);
         
     }
     return 0;
