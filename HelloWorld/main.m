a7365cc8c515bd007e972400a99d15665a77127e
Luciano Sousa <luciano@bonera.com.br>
2011-12-25 20:55:59 -0200

    added mutable string example

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -9,11 +9,17 @@ int main (int argc, const char * argv[])
         float value1 = 50.5f;
         int value2 = 20;
         sum = value1 + value2;
+        
+        //do not mutable
         NSString *firstString = @"'My first String'";
         
-        NSLog(@"The sum of the %f and %i is: %f", value1, value2, sum);
+        //mutable string
+        NSMutableString *secondString = [NSMutableString stringWithString: @"Our Second String was"];
+        [secondString appendString:@" created with a append."];
         
+        NSLog(@"The sum of the %f and %i is: %f", value1, value2, sum);        
         NSLog(@"Created a string: %@", firstString);
+        NSLog(@"Second string created with text: '%@'", secondString);
         
     }
     return 0;
