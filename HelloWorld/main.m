0ef93ea76259288d7938e216616cac1447b3cc22
Luciano Sousa <luciano@bonera.com.br>
2011-12-25 21:25:05 -0200

    added math operations examples

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -9,6 +9,9 @@ int main (int argc, const char * argv[])
         float value1 = 50.5f;
         int value2 = 20;
         sum = value1 + value2;
+        float mul = value1 * value2;
+        float div = value1 / value2;
+        float sub = value1 - value2;
         
         //do not mutable
         NSString *firstString = @"'My first String'";
@@ -17,7 +20,10 @@ int main (int argc, const char * argv[])
         NSMutableString *secondString = [NSMutableString stringWithString: @"Our Second String was"];
         [secondString appendString:@" created with a append."];
         
-        NSLog(@"The sum of the %f and %i is: %f", value1, value2, sum);        
+        NSLog(@"The sum of the %f and %i is: %f", value1, value2, sum);
+        NSLog(@"The multiplicated value is: %f", mul);
+        NSLog(@"The division value is: %f", div);
+        NSLog(@"The subtraction value is: %f", sub);
         NSLog(@"Created a string: %@", firstString);
         NSLog(@"Second string created with text: '%@'", secondString);
         
