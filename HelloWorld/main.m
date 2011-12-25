5cd09366fb50bc9c759a62635bf7fe72b4bfbab1
Luciano Sousa <luciano@bonera.com.br>
2011-12-25 20:29:03 -0200

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Luciano Sousa on 12/25/11.
+//  Copyright (c) 2011 Bonera Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
